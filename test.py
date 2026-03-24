b5cf2934cc938257583f5678c796629d10a5f8a4
Rajesh Tharyan <37782332+RajeshTharyan@users.noreply.github.com>
2026-03-24 23:34:18 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import streamlit
-import 
+import streamlit as sk
+import scikit-learn as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
